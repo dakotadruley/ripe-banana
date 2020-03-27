28700143505d1dda5d45a5f81e7ef68d0d66e0c3
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-03-27 09:54:26 -0700

    added all the actor stuff :)

--- __tests__/actors.test.js
+++ __tests__/actors.test.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-// const { getActor, getActors } = require('../db/data-helpers');
+const { getActor, getActors } = require('../db/data-helpers');
 const request = require('supertest');
 const app = require('../lib/app');
 
@@ -21,9 +21,45 @@ describe('Actor routes', () => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           name: 'Actor Test',
-          dob: 'December 17, 1985',
+          dob: '1985-12-17T08:00:00.000Z',
           pob: 'Test Place',
           __v: 0
+        });
+      });
+  });
+
+  it('gets all actors', async() => {
+    const actors = await getActors();
+
+    return request(app)
+      .get('/api/v1/actors')
+      .then(res => {
+        expect(res.body).toEqual(actors);
+      });
+  });
+
+  it('gets an actor by id', async() => {
+    const actor = await getActor();
+    // need filmss as well since we are populating in route
+    // const filmss = await getfilmss({ actorId: actor._id });
+
+    return request(app)
+      .get(`/api/v1/actors/${actor._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          ...actor,
+          // films - because we populate in route
+          // GET /actors/:id
+          // {
+          //     name,
+          //     dob,
+          //     pob,
+          //     films: [{
+          //       id,
+          //       title,
+          //       released
+          //     }]
+          // }
         });
       });
   });
