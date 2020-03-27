21130e4dd34624513318991841b5c3f498b287e6
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-03-27 14:48:36 -0700

    added the populate and films reference for studio routes

--- __tests__/studios.test.js
+++ __tests__/studios.test.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { getStudio, getStudios } = require('../db/data-helpers');
+const { getStudio, getStudios, getFilms } = require('../db/data-helpers');
 const request = require('supertest');
 const app = require('../lib/app');
 
@@ -14,11 +14,6 @@ describe('Studio routes', () => {
           state: 'CA',
           country: 'USA'
         }
-        // films: [{
-        //   id, 
-        //   title,
-        //   released
-        // }]
       })
       .then(res => {
         expect(res.body).toEqual({
@@ -44,28 +39,16 @@ describe('Studio routes', () => {
       });
   });
 
-  it('gets an actor by id', async() => {
+  it('gets a studio by id', async() => {
     const studio = await getStudio();
-    // need filmss as well since we are populating in route
-    // const filmss = await getfilmss({ actorId: actor._id });
+    const films = await getFilms({ 'studio': studio._id });
 
     return request(app)
       .get(`/api/v1/studios/${studio._id}`)
       .then(res => {
         expect(res.body).toEqual({
           ...studio,
-          // films - because we populate in route
-          // GET /actors/:id
-          // {
-          //     name,
-          //     dob,
-          //     pob,
-          //     films: [{
-          //       id,
-          //       title,
-          //       released
-          //     }]
-          // }
+          films
         });
       });
   });
